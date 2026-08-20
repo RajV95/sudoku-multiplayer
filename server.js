1ef58ff33989f5e37e9a773d6ef704c19c601db3
Rajvardhan <rajvardhan.ganji75@gmail.com>
2026-08-20 22:31:42 +0530

    feat: Add CI workflow for building and pushing Docker images to GitHub Container Registry

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ tsConfigPaths.register({
 });
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
+const hostname = process.env.HOST || '0.0.0.0';
 const port = parseInt(process.env.PORT || '3000', 10);
 
 const app = next({ dev, hostname, port });
